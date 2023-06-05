3ed711cfe74a837ba51067b83cd4035cf50cb739
Maria Gromyko <mazvereva91@mail.ru>
2023-06-05 13:10:52 +0300

    feat: add components: header, search, selector

---
+++ src/components/Controls.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Search } from "./Search";
+import { CustomSelect } from "./CustomSelect";
+
+const options = [
+  { value: "Africa", label: "Africa" },
+  { value: "America", label: "America" },
+  { value: "Asia", label: "Asia" },
+  { value: "Europe", label: "Europe" },
+  { value: "Oceania", label: "Oceania" },
+];
+
+// стилизация позиционирования
+const Wrapper=styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+
+@media(min-width: 767px){
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+`
+
+export const Controls = () => {
+  // переменная состояния для поиска
+  const [search, setSearch] = useState("");
+  // переменная состояния для выбора региона в селеткторе
+  const [region, setRegion] = useState("");
+  return (
+    <Wrapper>
+      <Search search={search} setSearch={setSearch} />
+      <CustomSelect
+        options={options}
+        placeholder="Filter by Region"
+        isClearable
+        isSearchable={false}
+        value={region}
+        onChange={setRegion}
+      />
+    </Wrapper>
+  );
+};
